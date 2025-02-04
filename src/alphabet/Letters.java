9c7377782a7b0027f74144407aecaf997e5b8e05
lenovo <lenovo@LAPTOP-MJ4J3446>
2025-02-04 17:42:22 -0600

    This is a program to print the alphabet one letter at a time.

---
+++ src/alphabet/Letters.java
@@ -0,0 +1,34 @@
+package alphabet;
+
+public class Letters {
+	
+	public static void main(String args[]) {
+
+		char letter = 'A';
+
+		switch (letter) {
+
+		case 'A':
+			System.out.println("This is letter: A.");
+			break;
+		case 'B':
+			System.out.println("This is letter: B.");
+			break;
+		case 'C':
+			System.out.println("This is letter: C.");
+			break;
+		case 'D':
+			System.out.println("This is letter: D.");
+			break;
+			
+			/**
+			 * Let's complete the alphabet. You can add 1 - 5 cases per push, so everyone in the team can participate.
+			 */
+
+		default:
+			System.out.println("This is not a letter from alphabet");
+		}
+
+	}
+
+}
