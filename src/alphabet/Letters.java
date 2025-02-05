acd38bd34dc9cf144730e051979d5237243ac312
lenovo <lenovo@LAPTOP-MJ4J3446>
2025-02-04 18:14:24 -0600

    We ready

--- src/alphabet/Letters.java
+++ src/alphabet/Letters.java
@@ -17,20 +17,6 @@ public class Letters {
 		case 'C':
 			System.out.println("This is letter: C.");
 			break;
-		case 'D':
-			System.out.println("This is letter: D.");
-			break;
-		case 'H':
-			System.out.println("This is letter: H.");
-			break;
-		case 'F':
-			System.out.println("This is letter: F.");
-			break;
-		case 'G':
-			System.out.println("This is letter: G.");
-			break;
-			
-			//Try
 			
 			/**
 			 * Let's complete the alphabet. You can add 1 - 5 cases per push, so everyone in the team can participate.
